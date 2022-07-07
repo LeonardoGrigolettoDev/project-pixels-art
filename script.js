dd46aa990884041cdf6d6b7d74b7de065d329cd3
LeonardoGrigolettoDev <leonardochawder@gmail.com>
2022-07-07 11:34:39 -0300

    captura cada pixel dentro do javascript

--- script.js
+++ script.js
@@ -37,6 +37,22 @@ function clicked (eventoDeOrigem) {
     eventoDeOrigem.target.classList.add("selected")
 }
 
+function paint (eventoDeOrigem){
+    let verify = document.getElementsByClassName("selected")[0];
+    if (verify === black) {
+        eventoDeOrigem.target.style.backgroundColor = "black"
+        }
+    if (verify === red) {
+        eventoDeOrigem.target.style.backgroundColor = "red"
+        }
+    if (verify === yellow) {
+        eventoDeOrigem.target.style.backgroundColor = "yellow"
+        }       
+    if (verify === blue) {
+        eventoDeOrigem.target.style.backgroundColor = "blue"
+        }
+}
+
 window.onload = function (){
     black.classList.add("selected")
 }
@@ -46,4 +62,29 @@ red.addEventListener("click", clicked);
 yellow.addEventListener("click", clicked);
 blue.addEventListener("click", clicked);
 
-console.log(black)
+td1.addEventListener("click", paint);
+td2.addEventListener("click", paint);
+td3.addEventListener("click", paint);
+td4.addEventListener("click", paint);
+td5.addEventListener("click", paint);
+td6.addEventListener("click", paint);
+td7.addEventListener("click", paint);
+td8.addEventListener("click", paint);
+td9.addEventListener("click", paint);
+td10.addEventListener("click", paint);
+td11.addEventListener("click", paint);
+td12.addEventListener("click", paint);
+td13.addEventListener("click", paint);
+td14.addEventListener("click", paint);
+td15.addEventListener("click", paint);
+td16.addEventListener("click", paint);
+td17.addEventListener("click", paint);
+td18.addEventListener("click", paint);
+td19.addEventListener("click", paint);
+td20.addEventListener("click", paint);
+td21.addEventListener("click", paint);
+td22.addEventListener("click", paint);
+td23.addEventListener("click", paint);
+td24.addEventListener("click", paint);
+td25.addEventListener("click", paint);
+
